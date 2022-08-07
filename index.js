a4183c868f099f492b6355d8f1fdfe1821f8739f
Shadan Khan <shadancs123@gmail.com>
2022-08-07 22:41:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+let player={
+    name:"SHADAN",
+    chips:123
+}
+let cards=[]
+let sum=0
+let hasBlackJack = false
+let isAlive=false
+let message=""
+let messageEl=document.getElementById("message-el")
+let sumEl=document.getElementById("sum-el")
+let cardEl=document.getElementById("card-el")
+
+let playerEl=document.getElementById("player-el")
+playerEl.textContent=player.name+" :  $"+player.chips
+
+function getRandomCard() {
+    let numbr= Math.floor(Math.random()*13)+1
+    if(numbr>10)
+        return 10
+    else if(numbr===1)
+        return 11
+    else    
+        return numbr
+}
+function startGame(){
+    isAlive=true
+    let firstCard=getRandomCard()
+    let secondCard=getRandomCard()
+    cards=[firstCard,secondCard]
+    sum=firstCard+secondCard    
+    renderGame()
+}
+function renderGame() {
+    cardEl.textContent="Cards: "
+    for(let i=0;i<cards.length;i++){
+        cardEl.textContent+=cards[i]+" "
+    }
+    sumEl.textContent="Sum: "+sum
+    if(sum===20){
+        message="Wohoo! you've got a BlackJack"
+        hasBlackJack=false
+    }
+    else if(sum<20){
+        message="DO u want to draw a new card? "
+    }
+    else{
+        message="You're out of the game!"
+        isAlive=false;
+    }
+    messageEl.textContent=message
+}
+function newCard() {
+    if (isAlive===true && hasBlackJack===false) {
+        let card=getRandomCard()
+        sum+=card
+        cards.push(card)
+        renderGame(card)
+    }
+}
